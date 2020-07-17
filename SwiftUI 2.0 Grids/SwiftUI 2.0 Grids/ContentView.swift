fdb72b263d1e955e24d6ae4b6444f0a29901551f
altyn <altmurzilka@gmail.com>
2020-07-18 00:02:50 +0600

    https://www.youtube.com/watch?v=kF4yFSiOee0

--- SwiftUI 2.0 Grids/SwiftUI 2.0 Grids/ContentView.swift
+++ SwiftUI 2.0 Grids/SwiftUI 2.0 Grids/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Алтын on 7/17/20.
 //
 
+/// https://www.youtube.com/watch?v=kF4yFSiOee0
+
 import SwiftUI
 
 struct ContentView: View {
@@ -21,22 +23,100 @@ struct ContentView_Previews: PreviewProvider {
 
 
 struct Home : View {
+    
+    @State var Languages : [Language] = [
+        Language(id: 0, lang: "English"),
+        Language(id: 1, lang: "Spanish"),
+        Language(id: 2, lang: "German"),
+        Language(id: 3, lang: "Japanese"),
+        Language(id: 4, lang: "Chinese"),
+        Language(id: 5, lang: "Korean"),
+        Language(id: 6, lang: "Kazakh"),
+    ]
+    
+    var columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 3)
+    
+    @Namespace var namespace
+    
+    @State var selected : [Language] = []
+    
     var body : some View {
-        ScrollView {
-            
+        
+        NavigationView {
+            ScrollView {
+                
+                if !self.Languages.isEmpty {
+                    LazyVGrid(columns: columns, spacing: 15) {
+                        
+                        ForEach(self.Languages) { language in
+                            Text(language.lang)
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .frame(width: 100, height: 100)
+                                .background(Color.blue)
+                                .cornerRadius(15)
+                                .matchedGeometryEffect(id: language.id, in: self.namespace)
+                                .onTapGesture {
+                                    self.selected.append(language)
+                                    
+                                    self.Languages.removeAll { (lang) ->
+                                        Bool in
+                                        
+                                        if lang.id == language.id {
+                                            return true
+                                        } else {
+                                            return false
+                                        }
+                                    }
+                                }
+                        }
+                    }
+                    .padding(.all)
+                }
+                
+                HStack {
+                    Text("Selected Language")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                LazyVGrid(columns: columns, spacing: 15) {
+                    
+                    ForEach(self.selected) { language in
+                        Text(language.lang)
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .frame(width: 100, height: 100)
+                            .background(Color.green)
+                            .cornerRadius(15)
+                            .matchedGeometryEffect(id: language.id, in: self.namespace)
+                            .onTapGesture {
+                                self.Languages.append(language)
+                                
+                                self.selected.removeAll { (lang) ->
+                                    Bool in
+                                    
+                                    if lang.id == language.id {
+                                        return true
+                                    } else {
+                                        return false
+                                    }
+                                }
+                            }
+                    }
+                }
+                .padding(.all)
+            }
+            .navigationTitle("Choose Language")
+            .navigationBarTitleDisplayMode(.inline)
         }
+        .animation(.easeOut)
     }
 }
 
-var Languages : [Language] = [
-    Language(id: 0, lang: "English"),
-    Language(id: 1, lang: "Spanish"),
-    Language(id: 2, lang: "German"),
-    Language(id: 3, lang: "Japanese"),
-    Language(id: 4, lang: "Chinese"),
-    Language(id: 5, lang: "Korean"),
-    Language(id: 6, lang: "Kazakh"),
-]
 
 struct Language : Identifiable {
     var id : Int
